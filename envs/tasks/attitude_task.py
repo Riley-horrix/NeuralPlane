f674d384725a53e0f30ad05ab2baff84e977fc41
rileyhorrix <riley@horrix.com>
2026-06-01 18:13:47 +0100

    fix: bruh

--- envs/tasks/attitude_task.py
+++ envs/tasks/attitude_task.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 from task_base import BaseTask
 from reward_functions.dallyverkampen_reward import DallyVerKampenReward
-from reward_functions.event_driven_reward import EventBasedReward, EventDrivenReward
+from reward_functions.event_driven_reward import EventDrivenReward
 from termination_conditions.unreach_posture import UnreachPosture
 from termination_conditions.overload import Overload
 from termination_conditions.high_speed import HighSpeed
